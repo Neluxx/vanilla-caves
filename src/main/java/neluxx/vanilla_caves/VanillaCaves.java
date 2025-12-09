e027b8d195b5dc729d575e56d98cd278ab9a18dd
Neluxx <fabian.arndt96@proton.me>
2025-12-09 08:24:56 +0100

    fix: set correct identifier in datapack registration

--- src/main/java/neluxx/vanilla_caves/VanillaCaves.java
+++ src/main/java/neluxx/vanilla_caves/VanillaCaves.java
@@ -6,32 +6,16 @@ import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class VanillaCaves implements ModInitializer {
 	public static final String MOD_ID = "vanilla-caves";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-
         // Register the datapack
         ResourceManagerHelper.registerBuiltinResourcePack(
-                Identifier.of("minecraft", "datapack"),
+                Identifier.of(MOD_ID, "datapack"),
                 FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
                 ResourcePackActivationType.ALWAYS_ENABLED
         );
-
-        LOGGER.info("Vanilla Caves datapack registered!");
 	}
 }
