19acb9b026f15b2d168edd5ac455be76abd8030f
Neluxx <fabian.arndt96@proton.me>
2026-04-15 19:56:45 +0200

    refactor: use utility class for beet plugins

--- src/plugins/cave_extra.py
+++ src/plugins/cave_extra.py
@@ -2,7 +2,7 @@ from beet import Context
 from beet.contrib.vanilla import Vanilla
 from beet.contrib.worldgen import WorldgenConfiguredCarver
 
-from src.plugins.utils import iterate_versions, parse_version
+from src.plugins.utils import iterate_versions, field_accessor
 
 
 def beet_default(ctx: Context):
@@ -12,25 +12,20 @@ def beet_default(ctx: Context):
         source = vanilla.releases[version].mount("data").data[WorldgenConfiguredCarver]
         patched = source["minecraft:cave"].copy()
         config = patched.data["config"]
-        nested = parse_version(version) < (1, 20, 5)
-
-        def field(name):
-            return config[name]["value"] if nested else config[name]
+        field = field_accessor(config, version)
 
         # The probability that each chunk attempts to generate carvers.
         config["probability"] = 0.2  # defaults to 0.07
 
         # Horizontally scales cave tunnels. Doesn't affect the length of tunnels.
-        x_radius = field("horizontal_radius_multiplier")
-        x_radius["max_exclusive"] = 1.5  # defaults to 1.4
-        x_radius["min_inclusive"] = 0.8  # defaults to 0.7
+        field("horizontal_radius_multiplier")["max_exclusive"] = 1.5  # defaults to 1.4
+        field("horizontal_radius_multiplier")["min_inclusive"] = 0.9  # defaults to 0.7
 
         # Vertically scales cave tunnels. Doesn't affect the length of tunnels.
-        y_radius = field("vertical_radius_multiplier")
-        y_radius["max_exclusive"] = 1.4  # defaults to 1.3
-        y_radius["min_inclusive"] = 0.9  # defaults to 0.8
+        field("vertical_radius_multiplier")["max_exclusive"] = 1.4  # defaults to 1.3
+        field("vertical_radius_multiplier")["min_inclusive"] = 1.0  # defaults to 0.8
 
         # The height at which this carver attempts to generate.
-        config["y"]["max_inclusive"]["absolute"] = 48  # defaults to 47
+        config["y"]["max_inclusive"]["absolute"] = 48  # defaults to 180
 
         pack[WorldgenConfiguredCarver]["minecraft:cave"] = patched
